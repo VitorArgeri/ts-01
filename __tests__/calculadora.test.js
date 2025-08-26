bb11d15702e56f312ceccc863a1bbb6cac4c7bad
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-08-26 10:33:23 -0300

    feat: create functions and tests

---
+++ __tests__/calculadora.test.js
@@ -0,0 +1,49 @@
+import { somar, subtrair, multiplicar, dividir, ehPar } from "../src/calculadora.js"
+
+describe("Calculadora", () => {
+    test("Deve somar dois números corretamente", () => {
+        // AAA
+        // Arrange (Preparar)
+        const a = 2
+        const b = 3
+        // Act (Executar)
+        const resultado = somar(a, b)
+        // Assert (Verificar)
+        expect(resultado).toBe(5)
+    })
+})
+
+describe("Calculadora", () => {
+    test("Deve subtrair dois números corretamente", () => {
+        const a = 2
+        const b = 3
+        const resultado = subtrair(a, b)
+        expect(resultado).toBe(-1)
+    })
+})
+
+describe("Calculadora", () => {
+    test("Deve multiplicar dois números corretamente", () => {
+        const a = 2
+        const b = 3
+        const resultado = multiplicar(a, b)
+        expect(resultado).toBe(6)
+    })
+})
+
+describe("Calculadora", () => {
+    test("Deve dividir dois números corretamente", () => {
+        const a = 3
+        const b = 3
+        const resultado = dividir(a, b)
+        expect(resultado).toBe(1)
+    })
+})
+
+describe("Calculadora", () => {
+    test("Deve verificar se o número é par", () => {
+        const a = 2
+        const resultado = ehPar(a)
+        expect(resultado).toBe("É par")
+    })
+})
---
+++ src/calculadora.js
@@ -0,0 +1,27 @@
+// Função simples para testar soma
+export function somar(a, b) {
+    return a + b
+}
+
+// Função simples para testar subtração
+export function subtrair(a, b) {
+    return a - b
+}
+
+// Função simples para testar multiplicação
+export function multiplicar(a, b) {
+    return a * b
+}
+
+// Função simples para testar divisão
+export function dividir(a, b) {
+    return a / b
+}
+
+export function ehPar(a) {
+    if(a % 2 == 0) {
+        return "É par"
+    } else {
+        return "É ímpar"
+    }
+}
